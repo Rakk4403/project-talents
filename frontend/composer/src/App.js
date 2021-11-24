aad5d90f559e5e45abbe64a461161fceced9635a
chris-noul <chris@noul.kr>
2021-11-24 13:52:01 +0900

    멤버바구니에서 제거

--- frontend/composer/src/App.js
+++ frontend/composer/src/App.js
@@ -5,41 +5,42 @@ import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import {ItemTypes} from "./data/types";
 
-function App({ data }) {
-
+function App({data}) {
   const keys = Object.keys(data);
-  const groups = keys.filter((key) => data[key].type === ItemTypes.Group);
-  const members = keys.filter((key) => data[key].type === ItemTypes.Member);
+  const groupKeys = keys.filter((key) => data[key].type === ItemTypes.Group);
+  const memberKeys = keys.filter((key) => data[key].type === ItemTypes.Member);
 
   return (
-    <DndProvider backend={HTML5Backend} >
-    <div className="App">
-      {groups.map((key) => {
-        const value = data[key];
-        const members = Object.values(data).filter((elem) => elem.parent === key);
-        return (
-          <Group
-            // data={data}
-            key={key}
-            groupId={key}
-            title={value.title}
-            members={members}
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        {groupKeys.map((key) => {
+          const value = data[key];
+          const members = Object.values(data).filter((elem) => elem.parent === key);
+          return (
+            <Group
+              // data={data}
+              key={key}
+              groupId={key}
+              title={value.title}
+              members={members}
             />
-        )
-      })}
-      All Members:
-      {members.map((key) => {
-        const value = data[key];
-        return (
-          <Member
-            data={data}
-            key={key}
-            memberId={key}
-            title={value.title}
-            />
-        )
-      })}
-    </div>
+          )
+        })}
+        All Members:
+        {memberKeys
+          .filter(memberId => !data[memberId].parent)
+          .map((key) => {
+            const value = data[key];
+            return (
+              <Member
+                data={data}
+                key={key}
+                memberId={key}
+                title={value.title}
+              />
+            )
+          })}
+      </div>
     </DndProvider>
   );
 }
