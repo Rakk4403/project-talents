3695585bb9bba9527a548d7ac56bd5a99c4dff26
Choi Rak-Kyoung <rakk4403@gmail.com>
2022-01-19 23:01:41 +0900

    패널 확장축소.. 어떻게 예쁘게 할 수 있을까

--- frontend/composer/src/ControlPanel.jsx
+++ frontend/composer/src/ControlPanel.jsx
@@ -4,94 +4,120 @@ import {ItemTypes} from "./data/types";
 import Group from "./component/Group";
 import Talent from "./component/Talent";
 import Draggable from "react-draggable";
+import {useState} from "react";
 
 function ControlPanel({data}) {
+  const [openDetails, setOpenDetails] = useState(true);
   const keys = Object.keys(data);
   const talentKeys = keys.filter((key) => data[key].type === ItemTypes.Talent);
+  const toggleDetails = () => {
+    setOpenDetails(!openDetails);
+  }
   return (
     <Draggable
       handle=".panelHandle"
-      style={{position: 'fixed'}}
-    >
-      <div style={{
-        border: 'solid',
-        borderColor: 'lightgray',
-        borderRadius: 10,
-        borderWidth: 1,
-        boxShadow: "grey 1px 1px 5px",
-        backgroundColor: 'aliceblue',
-        width: '20%',
+      style={{
+        position: 'fixed',
         minWidth: 250,
-        maxWidth: 330,
-        height: 800,
-        display: 'flex',
-        flexFlow: 'column',
-        gap: 10,
-      }}>
-        <div
-          className="panelHandle"
-          style={{
-            backgroundColor: 'gray',
-            height: '3%',
-            borderTopRightRadius: 10,
-            borderTopLeftRadius: 10,
-            cursor: 'move',
-          }}>
+      }}
+    >
+      <div>
+        <div style={{width: 50, height: 50,}}>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleDetails();
+            }}
+          >확장축소
+          </button>
         </div>
-        <div style={{
-          display: 'flex',
-          flexFlow: 'column',
-          height: '95%',
-          gap: 10,
-          padding: 10,
-        }}>
+        <details open={openDetails}>
+          <summary style={{listStyle: 'none', display: openDetails && 'none'}}/>
+          <div
+            className="panelHandle"
+            style={{
+              backgroundColor: 'gray',
+              height: 20,
+              width: 250,
+              borderTopRightRadius: 10,
+              borderTopLeftRadius: 10,
+              borderWidth: 1,
+              boxShadow: "grey 1px 1px 5px",
+              cursor: 'move',
+              listStyle: 'none',
+            }}>
+          </div>
           <div style={{
-            display: 'flex', justifyContent: 'center',
-            alignItems: 'center'
+            border: 'solid',
+            borderColor: 'lightgray',
+            borderBottomLeftRadius: 10,
+            borderBottomRightRadius: 10,
+            borderWidth: 1,
+            boxShadow: "grey 1px 1px 5px",
+            backgroundColor: 'aliceblue',
+            width: 250,
+            height: 800,
+            display: 'flex',
+            flexFlow: 'column',
+            gap: 10,
           }}>
-            <WasteBox data={data}/>
+
+            <div style={{
+              display: 'flex',
+              flexFlow: 'column',
+              height: '95%',
+              gap: 10,
+              padding: 10,
+            }}>
+              <div style={{
+                display: 'flex', justifyContent: 'center',
+                alignItems: 'center'
+              }}>
+                <WasteBox data={data}/>
+              </div>
+              <div style={{display: 'flex', justifyContent: 'space-between', margin: 5}}>
+                <button
+                  style={{height: 40}}
+                  onClick={() => addElem(ItemTypes.Group)}>Add Group
+                </button>
+                <button
+                  style={{height: 40}}
+                  onClick={() => addElem(ItemTypes.Member)}>Add Member
+                </button>
+                <button
+                  style={{height: 40}}
+                  onClick={() => addElem(ItemTypes.Talent)}>Add Talent
+                </button>
+              </div>
+              <span style={{fontWeight: 'bold'}}>Members</span>
+              <div style={{display: 'flex', height: '50%'}}>
+                <Group
+                  disableBubbleChart
+                  disableDrag
+                  disableDrop
+                  style={{height: '100%', width: '100%'}}
+                  data={data}
+                />
+              </div>
+              <span style={{fontWeight: 'bold'}}>Talents</span>
+              <div style={{display: 'flex', flexWrap: 'wrap', overflow: 'auto'}}>
+                {talentKeys.map((key) =>
+                  <Talent
+                    key={key}
+                    title={data[key].title}
+                    color={data[key].color}
+                    talentId={key}
+                    style={{
+                      height: 30,
+                      flexWrap: 'wrap',
+                      overflow: 'hidden',
+                    }}
+                    tooltip={data[key].title}
+                  />)}
+              </div>
+            </div>
           </div>
-          <div style={{display: 'flex', justifyContent: 'space-between', margin: 5}}>
-            <button
-              style={{height: 40}}
-              onClick={() => addElem(ItemTypes.Group)}>Add Group
-            </button>
-            <button
-              style={{height: 40}}
-              onClick={() => addElem(ItemTypes.Member)}>Add Member
-            </button>
-            <button
-              style={{height: 40}}
-              onClick={() => addElem(ItemTypes.Talent)}>Add Talent
-            </button>
-          </div>
-          <span style={{fontWeight: 'bold'}}>Members</span>
-          <div style={{display: 'flex', height: '50%'}}>
-            <Group
-              disableBubbleChart
-              disableDrag
-              disableDrop
-              style={{height: '100%', width: '100%'}}
-              data={data}
-            />
-          </div>
-          <span style={{fontWeight: 'bold'}}>Talents</span>
-          <div style={{display: 'flex', flexWrap: 'wrap', overflow: 'auto'}}>
-            {talentKeys.map((key) =>
-              <Talent
-                key={key}
-                title={data[key].title}
-                color={data[key].color}
-                talentId={key}
-                style={{
-                  height: 30,
-                  flexWrap: 'wrap',
-                  overflow: 'hidden',
-                }}
-                tooltip={data[key].title}
-              />)}
-          </div>
-        </div>
+        </details>
       </div>
     </Draggable>
   )
