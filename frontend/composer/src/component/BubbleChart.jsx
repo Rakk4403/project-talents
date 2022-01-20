3681a1172a34ecc563fa2515c023296a1c98b8d5
Choi Rak-Kyoung <rakk4403@gmail.com>
2022-01-20 21:54:32 +0900

    그렇다 정답은 로그10이었다

--- frontend/composer/src/component/BubbleChart.jsx
+++ frontend/composer/src/component/BubbleChart.jsx
@@ -85,7 +85,7 @@ class BubbleChart extends React.Component {
   radiusScale = value => {
     const {data} = this.props;
     const sumValue = d3.sum(data, item => item.v)
-    const ratio = 1 / sumValue.toString().length
+    const ratio = 1 / Math.log10(sumValue)
     return (30 + value * 5) * ratio;
   };
 
